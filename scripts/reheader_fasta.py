62fb84d52be86a0dc3f3fb33f9466875cb580988
Andrius Daunoravicius <andrius.daunoravicius@505hq.com>
2020-11-18 12:18:33 +0200

    SAP-18535: extended fasta reheadering script to include sequence lenghts https://jira.congenica.net/browse/SAP-18535

--- scripts/reheader_fasta.py
+++ scripts/reheader_fasta.py
@@ -1,9 +1,9 @@
 # genome sequences produce by ncov have sequence identifiers that include
 # QC parameters. This is to get rid of them.
 
-import os
 import re
 import sys
+from os.path import join
 from pathlib import Path
 
 from Bio import SeqIO
@@ -12,9 +12,11 @@ from Bio.SeqRecord import SeqRecord
 FASTQ_FILE_EXTENSION = "fa"
 FASTQ_FILE_HANDLE = "fasta"
 SEQUENCE_DESCRIPTION = "SARS-CoV-2"
+ASSEMBLY_LENGTHS_FILENAME = "sequence_lengths.text"
 
 
-def convert_file(source_file, output_dir):
+def convert_file(source_file: Path, output_dir: Path):
+    sequence_lengths = {}
     for record in SeqIO.parse(source_file, FASTQ_FILE_HANDLE):
         # >Consensus_ERR4157960.primertrimmed.consensus_threshold_0.75_quality_20
         sample_name = re.search(r"^Consensus_(\w+)", record.id)
@@ -25,12 +27,20 @@ def convert_file(source_file, output_dir):
             continue
 
         sample_name = sample_name.group(1)
-        output_file = os.path.join(output_dir, f"{sample_name}.{FASTQ_FILE_HANDLE}")
+
+        sequence_lengths[sample_name] = len(record.seq)
+        output_file = join(output_dir, f"{sample_name}.{FASTQ_FILE_HANDLE}")
         with open(output_file, "w") as output:
             new_record = SeqRecord(
                 record.seq, id=sample_name, description=SEQUENCE_DESCRIPTION
             )
             SeqIO.write(new_record, output, FASTQ_FILE_HANDLE)
+
+    # write gene assembly lengths to file, for use in Nextstrain
+    lengths_file = join(output_dir, ASSEMBLY_LENGTHS_FILENAME)
+    with open(lengths_file, 'a') as lengths_file:
+        for sequence, length in sequence_lengths.items():
+            lengths_file.write(f'{sequence}\t{length}\n')
 
 
 if __name__ == "__main__":
@@ -47,4 +57,4 @@ if __name__ == "__main__":
 
     for path in Path(sys.argv[1]).rglob(f"*.{FASTQ_FILE_EXTENSION}"):
         print(f"processing file {path}")
-        convert_file(path, destination)
+        convert_file(path, Path(destination))
