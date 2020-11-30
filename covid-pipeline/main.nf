c60ef586a31744ad0a2428571a6488464a5dfea8
Tautvydas Petkus <tautvydas.petkus@telesofas.com>
2020-11-30 12:19:28 +0200

    SAP-18279 Generate tsv for nextstrain python script
    Also included prototype for integration part

--- covid-pipeline/main.nf
+++ covid-pipeline/main.nf
@@ -29,6 +29,7 @@ required_variable = DB_PASSWORD
 // Import modules
 include { ncov2019_artic_nf_pipeline } from './modules.nf'
 include { load_ncov_assembly_qc_to_db } from './modules.nf'
+include { prepare_tsv_for_nextstrain } from './modules.nf'
 include { reheader_genome_fasta } from './modules.nf'
 include { pangolin_pipeline } from './modules.nf'
 include { load_pangolin_data_to_db } from './modules.nf'
@@ -41,7 +42,7 @@ workflow {
         params.ncov_prefix
     )
 
-    load_ncov_assembly_qc_to_db(
+    ch_ncov_qc_sample_submitted = load_ncov_assembly_qc_to_db(
         ncov2019_artic_nf_pipeline.out.ch_qc_csv_ncov_result,
         ncov2019_artic_nf_pipeline.out.ch_sample_depth_ncov_results
     )
@@ -57,4 +58,7 @@ workflow {
     load_pangolin_data_to_db(
         pangolin_pipeline.out.ch_pangolin_lineage_csv
     )
+
+    // TODO: add pangolin submit to database dependency for prepare_tsv_for_nextstrain
+    ch_nextstrain_input_tsv = prepare_tsv_for_nextstrain(ch_ncov_qc_sample_submitted.collect())
 }
---
+++ scripts/generate_nextstrain_input_tsv.py
@@ -0,0 +1,99 @@
+from typing import List
+from datetime import datetime
+from dataclasses import dataclass
+import csv
+
+import click
+from sqlalchemy.orm import joinedload
+
+from bahrain_covid.database import session_handler
+from bahrain_covid.models import Sample
+
+
+@dataclass
+class NextstrainSampleMetadataInput:
+    strain: str
+    date: datetime
+    division: str
+    area: str
+    country_exposure: str
+    length: int
+    age: int
+    sex: str
+    pangolin_lineage: str
+    nationality: str
+    ct_value: float
+    symptoms: str
+    comorbitities: str
+    travel_exposure: str
+    hospital_admittance: str
+
+    gisaid_epi_isl = None
+    genbank_accession = None
+    region_exposure = None
+    division_exposure = None
+    authors = None
+    title = None
+    date_submitted = None
+    virus: str = "SARS-CoV-2"
+    region: str = "Asia"
+    country: str = "Bahrain"
+    segment: str = "genome"
+    host: str = "human"
+    originating_lab: str = "Bahrain Public Health Laboratory"
+    submitting_lab: str = "Bahrain Public Health Laboratory"
+
+
+def get_data_for_nextstrain() -> List[NextstrainSampleMetadataInput]:
+    res = []
+    with session_handler() as session:
+        samples = session.query(Sample).options(joinedload(Sample.comorbidities)).all()
+        for sample in samples:
+            res.append(
+                NextstrainSampleMetadataInput(
+                    strain=sample.lab_id,
+                    date=sample.date_collected,
+                    division=sample.governorate_name,
+                    area=sample.area_name,
+                    country_exposure=sample.travel_exposure,
+                    length=sample.genome_length,
+                    age=sample.age,
+                    sex=sample.gender,
+                    pangolin_lineage=sample.pangolin_lineage,
+                    nationality=sample.nationality,
+                    ct_value=sample.ct_value,
+                    symptoms=sample.symptoms,
+                    comorbitities=",".join([x.description for x in sample.comorbidities]),
+                    travel_exposure=sample.travel_exposure,
+                    hospital_admittance=sample.hospital_admittance,
+                )
+            )
+    return res
+
+
+@click.command()
+@click.option(
+    "--output",
+    type=click.Path(file_okay=True, writable=True),
+    required=True,
+    help="ncov pipeline resulting qc csv file",
+)
+def generate_nextstrain_input_tsv(output: str) -> None:
+    """
+    Generate a tsv file from a database, which will be used in nextstrain pipeline
+    """
+    samples = get_data_for_nextstrain()
+
+    with open(output, "w") as out_file:
+        tsv_writer = csv.writer(out_file, delimiter="\t")
+        header_keys = None
+        for sample in samples:
+            if header_keys is None:
+                header_keys = sample.__dict__.keys()
+                tsv_writer.writerow(header_keys)
+            tsv_writer.writerow([sample.__getattribute__(key) for key in header_keys])
+
+
+if __name__ == "__main__":
+    # pylint: disable=no-value-for-parameter
+    generate_nextstrain_input_tsv()
