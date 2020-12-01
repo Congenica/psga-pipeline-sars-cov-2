640573c4f8314ee38cdf839f13e91f75ebb57a5f
Andrew Bayliss <andrew.bayliss@congenica.com>
2020-12-01 11:30:50 +0000

    SAP-18278 populate governorate and area tables

---
+++ sqitch/revert/05-governorates.sql
@@ -0,0 +1,14 @@
+-- Revert ps-bahrain-covid:05-governorates from pg
+
+BEGIN;
+
+  SET LOCAL search_path = sars_cov_2;
+
+  DELETE FROM governorate WHERE name in (
+    'Capital'::governorate_name,
+    'Northern'::governorate_name,
+    'Southern'::governorate_name,
+    'Muharraq'::governorate_name
+  );
+
+COMMIT;
