9a58b1fe149b188e6dc44ec97a8e32b5717a9d1e
Andrew Bayliss <andrew.bayliss@congenica.com>
2020-12-03 16:19:16 +0000

    SAP-18285 add the colour information for areas

---
+++ sqitch/verify/08-add_area_colours.sql
@@ -0,0 +1,10 @@
+-- Verify ps-bahrain-covid:08-add_area_colours on pg
+
+DO $$
+DECLARE
+  res int;
+BEGIN
+  SET LOCAL search_path = sars_cov_2;
+  res := (SELECT COUNT(*) FROM area WHERE colour IS NOT NULL);
+  ASSERT res = 176;
+END $$;
