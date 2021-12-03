a379e41b5818c56669a12f0740f87367e635df45
Piero Dalle Pezze <piero.dallepezze@congenica.com>
2021-12-03 00:44:05 +0000

    PSG-154: remove nextstrain, concatenate_fasta, microreact and reports.

--- sqitch/revert/03-add-pangolin-probability.sql
+++
@@ -1,9 +0,0 @@
--- Revert covid-pipeline:03-add-pangolin-probability from pg
-
-BEGIN;
-
-    SET LOCAL search_path = sars_cov_2;
-
-    ALTER TABLE "sample" DROP COLUMN IF EXISTS "pangolin_probability";
-
-COMMIT;
