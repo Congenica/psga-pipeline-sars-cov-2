af1411fb7ef0c1732502449de2bf0651bac55703
Piero Dalle Pezze <piero.dallepezze@congenica.com>
2022-02-14 13:27:01 +0000

    PSG-183: improve set up and tear down of minikube environment

--- minikube/startup.sh
+++ minikube/startup.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 wait_for_pod() {
     local __POD="${1}"
 
@@ -11,29 +10,24 @@ wait_for_pod() {
     printf "\n${__POD} is running\n"
 }
 
-# set new namespace
+# create new namespace and set it as default
 kubectl apply -f create_namespace.yaml
 kubectl config set-context $(kubectl config current-context) --namespace=ukhsa-covid
 
 # set service account
 kubectl apply -f service_account.yaml
 
-# I think this is no longer required as we use the same service account config as in congenica k8s cluster
-## create rolebinding. e.g. kubectl create rolebinding default-edit --clusterrole=edit --serviceaccount=<namespace>:default
-#kubectl create rolebinding default-edit --clusterrole=edit --serviceaccount=ukhsa-covid:ukhsa-covid-admin
-#kubectl create rolebinding default-view --clusterrole=view --serviceaccount=ukhsa-covid:ukhsa-covid-admin
-
 
 ## deploy DB. Currently, the DB is stored in a pod.
 # This is fine for a proof of concept, but obviously not the long term solution.
 # The DB will be stored in a RDS aurora system
-kubectl create -f deploy_db.yaml
+kubectl apply -f deploy_db.yaml
 db_pod="$( kubectl get pods -l app=psql --no-headers -o custom-columns=':metadata.name' )"
 wait_for_pod "${db_pod}"
 kubectl exec -it ${db_pod} -- bash -c './setup_db.sh'
 
 ## deploy covid-pipeline
-kubectl create -f deploy_covid_pipeline.yaml
+kubectl apply -f deploy_covid_pipeline.yaml
 pipeline_pod="$( kubectl get pods -l app=covid-pipeline --no-headers -o custom-columns=':metadata.name' )"
 wait_for_pod "${pipeline_pod}"
 # you will need to copy files to the /data/input or change COVID_PIPELINE_INPUT_PATH to point to an s3 location
