d5f0077b842b268323d9313af92741bca974e634
Piero Dalle Pezze <piero.dallepezze@congenica.com>
2022-02-18 12:08:02 +0000

    PSG-223: remove tables and columns that are not required for this project

--- sqitch/deploy/02-apptables.sql
+++ sqitch/deploy/02-apptables.sql
@@ -7,122 +7,17 @@ BEGIN;
 
   SET LOCAL search_path = sars_cov_2;
 
-  CREATE TYPE "gender" AS ENUM (
-    'M'
-   ,'F'
-   ,'U'
-  );
-
-  CREATE TYPE "governorate_name" AS ENUM (
-    'Capital'
-   ,'Northern'
-   ,'Southern'
-   ,'Muharraq'
-  );
-
-  CREATE TYPE "hospital_admittance" AS ENUM (
-    'No'
-   ,'Standard'
-   ,'ICU'
-  );
-
-  -- NOTE: If geo coords are used often, replace lat/long with PostGIS
-  -- https://postgis.net/install/
-  -- Initially PostGIS extension not used for simplicity
-  CREATE TABLE IF NOT EXISTS "area" (
-    "name"      VARCHAR PRIMARY KEY
-   ,"latitude"  DOUBLE PRECISION
-   ,"longitude"	DOUBLE PRECISION
-   ,"colour"  	BYTEA
-  );
-
-  COMMENT ON TABLE "area" IS
-    'Geographic "Area" location';
-    COMMENT ON COLUMN "area"."name" IS
-      'Primary key, the area name, low level geolocation from where the sample was collected';
-    COMMENT ON COLUMN "area"."latitude" IS
-      'The angular distance of the location north or south of the equator';
-    COMMENT ON COLUMN "area"."longitude" IS
-      'the angular distance of the location east or west of the Greenwich meridian';
-    COMMENT ON COLUMN "area"."colour" IS
-      'The colour of this block in reports';
-
-  CREATE TABLE IF NOT EXISTS "governorate" (
-    "name"      "governorate_name" PRIMARY KEY
-   ,"latitude"  DOUBLE PRECISION
-   ,"longitude" DOUBLE PRECISION
-   ,"colour"    BYTEA
-  );
-
-  COMMENT ON TABLE "governorate" IS
-    'Geographic "Governorate" location';
-    COMMENT ON COLUMN "area"."name" IS
-      'Primary key, the area name, capitol level geolocation from where the sample was collected';
-    COMMENT ON COLUMN "area"."latitude" IS
-      'The angular distance of the location north or south of the equator';
-    COMMENT ON COLUMN "area"."longitude" IS
-      'the angular distance of the location east or west of the Greenwich meridian';
-    COMMENT ON COLUMN "area"."colour" IS
-      'The colour of this block in reports';
-
-  CREATE TABLE IF NOT EXISTS "block" (
-    "number"    VARCHAR PRIMARY KEY
-   ,"latitude"  DOUBLE PRECISION
-   ,"longitude" DOUBLE PRECISION
-   ,"colour"    BYTEA
-  );
-
-  COMMENT ON TABLE "block" IS
-    'Geographic "Block" location';
-    COMMENT ON COLUMN "block"."number" IS
-      'Primary key, the block number, low level geolocation from where the sample was collected';
-    COMMENT ON COLUMN "block"."latitude" IS
-      'The angular distance of the location north or south of the equator';
-    COMMENT ON COLUMN "block"."longitude" IS
-      'the angular distance of the location east or west of the Greenwich meridian';
-    COMMENT ON COLUMN "block"."colour" IS
-      'The colour of this block in reports';
-
-  CREATE TABLE IF NOT EXISTS "comorbidity" (
-    "comorbidity_id" INTEGER PRIMARY KEY
-   ,"description"    TEXT
-  );
-
-  COMMENT ON TABLE "comorbidity" IS
-    'Coded diseases presented in patient (eg Diabetes)';
-    COMMENT ON COLUMN "comorbidity"."comorbidity_id" IS
-      'Primary key';
-    COMMENT ON COLUMN "comorbidity"."description" IS
-      'Description of the comorbidity';
-
   CREATE TABLE IF NOT EXISTS "sample" (
     "sample_id"      SERIAL PRIMARY KEY
    ,"lab_id"         VARCHAR
    ,"date_collected" TIMESTAMP WITHOUT TIME ZONE
    ,"data_sequenced" TIMESTAMP WITHOUT TIME ZONE
    ,"sequencing_run" VARCHAR
-   ,"gender"	     "gender" NOT NULL DEFAULT 'U'
-   ,"age"            INTEGER -- Why not DOB?
-   ,"nationality"    VARCHAR
-   ,"governorate_name"    "governorate_name"
-      REFERENCES "governorate" ("name")
-      ON DELETE NO ACTION ON UPDATE NO ACTION DEFERRABLE INITIALLY DEFERRED
-   ,"area_name"      VARCHAR
-      REFERENCES "area" ("name")
-      ON DELETE NO ACTION ON UPDATE NO ACTION DEFERRABLE INITIALLY DEFERRED
-   ,"block_number"   VARCHAR
-      REFERENCES     "block" ("number")
-      ON DELETE NO ACTION ON UPDATE NO ACTION DEFERRABLE INITIALLY DEFERRED
    ,"sample_number"  VARCHAR
-   ,"ct_value"       DOUBLE PRECISION
-   ,"symptoms"       TEXT
-   ,"travel_exposure"  VARCHAR
-   ,"hospital_admittance" "hospital_admittance"
    ,"pangolin_lineage" VARCHAR
    ,"genbank_id"     INTEGER
    ,"gisaid_id"      VARCHAR
    ,"genome_length"  INTEGER
-   ,"mrn"            INTEGER
   );
 
   COMMENT ON TABLE "sample" IS
@@ -137,28 +32,8 @@ BEGIN;
       'timestamp of sample sequencing';
     COMMENT ON COLUMN "sample"."sequencing_run" IS
       'identifier of sequencing run';
-    COMMENT ON COLUMN "sample"."gender" IS
-      'Gender of host person';
-    COMMENT ON COLUMN "sample"."age" IS
-      'Age of host person in years';
-    COMMENT ON COLUMN "sample"."nationality" IS
-      'Nationality of host person';
-    COMMENT ON COLUMN "sample"."governorate_name" IS
-      'high level geolocation from where sample was collected';
-    COMMENT ON COLUMN "sample"."area_name" IS
-      'foreign key to area table, mid level geolocation from where sample was collected';
-    COMMENT ON COLUMN "sample"."block_number" IS
-      'foreign key to block table, low level geolocation from where sample was collected';
     COMMENT ON COLUMN "sample"."sample_number" IS
       'Lab sample identifier (may be redundant with lab_id)';
-    COMMENT ON COLUMN "sample"."ct_value" IS
-      'RNA quality measure';
-    COMMENT ON COLUMN "sample"."symptoms" IS
-      'Symptoms observed';
-    COMMENT ON COLUMN "sample"."travel_exposure" IS
-      'Country host visited recently';
-    COMMENT ON COLUMN "sample"."hospital_admittance" IS
-      'Hospital type host admited to';
     COMMENT ON COLUMN "sample"."pangolin_lineage" IS
       'Viral phylogenetic lineage';
     COMMENT ON COLUMN "sample"."genbank_id" IS
@@ -167,8 +42,6 @@ BEGIN;
       'GISAID identifier (GISAID is a global science initiative and primary source that provides open-access to genomic data of the novel coronavirus responsible for COVID-19)';
     COMMENT ON COLUMN "sample"."genome_length" IS
       'Number of base pair in the virus genome';
-    COMMENT ON COLUMN "sample"."mrn" IS
-      'Individual National Identity number';
 
 
   CREATE TABLE IF NOT EXISTS "sample_qc" (
@@ -202,24 +75,5 @@ BEGIN;
       'QC plot image';
     COMMENT ON COLUMN "sample_qc"."pipeline_version" IS
       'mapping pipeline version';
-
-
-  CREATE TABLE IF NOT EXISTS "sample_comorbidity" (
-    "sample_id" INTEGER
-      REFERENCES "sample" ("sample_id")
-      ON DELETE NO ACTION ON UPDATE NO ACTION DEFERRABLE INITIALLY DEFERRED
-   ,"comorbidity_id" INTEGER
-      REFERENCES "comorbidity" ("comorbidity_id")
-      ON DELETE NO ACTION ON UPDATE NO ACTION DEFERRABLE INITIALLY DEFERRED
-   ,PRIMARY KEY ("sample_id", "comorbidity_id")
-  );
-
-  COMMENT ON TABLE "sample_comorbidity" IS
-    'Sample comorbitiy, a linking table between the sample an comorbidity record';
-    COMMENT ON COLUMN "sample_comorbidity"."sample_id" IS
-      'Primary key with comorbidity_id, and foreign key to sample table';
-    COMMENT ON COLUMN "sample_comorbidity"."comorbidity_id" IS
-      'Primary key with sample_id, and foreign key to comorbidity table';
-
 
 COMMIT;
--- sqitch/verify/02-apptables.sql
+++ sqitch/verify/02-apptables.sql
@@ -7,32 +7,11 @@ BEGIN
 
   SET LOCAL search_path = sars_cov_2;
 
-  -- area table verifications
-  ASSERT (SELECT has_table_privilege('area', 'SELECT, INSERT, UPDATE, DELETE, TRUNCATE, REFERENCES'));
-  SELECT count(*) INTO column_count
-  FROM information_schema.columns
-  WHERE table_schema='sars_cov_2' AND table_name='area' AND column_name IN ('name','latitude','longitude','colour');
-  ASSERT column_count = 4;
-
-  -- block table verifications
-  ASSERT (SELECT has_table_privilege('block', 'SELECT, INSERT, UPDATE, DELETE, TRUNCATE, REFERENCES'));
-  SELECT count(*) INTO column_count
-  FROM information_schema.columns
-  WHERE table_schema='sars_cov_2' AND table_name='block' AND column_name IN ('number','latitude','longitude','colour');
-  ASSERT column_count = 4;
-
-    -- comorbidity table verifications
-  ASSERT (SELECT has_table_privilege('comorbidity', 'SELECT, INSERT, UPDATE, DELETE, TRUNCATE, REFERENCES'));
-  SELECT count(*) INTO column_count
-  FROM information_schema.columns
-  WHERE table_schema='sars_cov_2' AND table_name='comorbidity' AND column_name IN ('comorbidity_id','description');
-  ASSERT column_count = 2;
-
     -- sample table verifications
   ASSERT (SELECT has_table_privilege('sample', 'SELECT, INSERT, UPDATE, DELETE, TRUNCATE, REFERENCES'));
   SELECT count(*) INTO column_count
   FROM information_schema.columns
-  WHERE table_schema='sars_cov_2' AND table_name='sample' AND column_name IN ('sample_id', 'lab_id', 'date_collected', 'data_sequenced', 'sequencing_run', 'gender', 'age', 'nationality', 'governorate_name', 'area_name', 'block_number', 'sample_number', 'ct_value', 'symptoms', 'travel_exposure', 'hospital_admittance', 'pangolin_lineage', 'gisaid_id', 'genome_length', 'mrn');
+  WHERE table_schema='sars_cov_2' AND table_name='sample' AND column_name IN ('sample_id', 'lab_id', 'date_collected', 'data_sequenced', 'sequencing_run', 'sample_number', 'pangolin_lineage', 'gisaid_id', 'genome_length');
   ASSERT column_count = 20;
 
     -- sample_qc table verifications
@@ -41,19 +20,5 @@ BEGIN
   FROM information_schema.columns
   WHERE table_schema='sars_cov_2' AND table_name='sample_qc' AND column_name IN ('sample_id', 'pct_covered_bases', 'num_aligned_reads', 'qc_pass', 'qc_plot', 'pipeline_version');
   ASSERT column_count = 6;
-
-    -- sample_comorbidity table verifications
-  ASSERT (SELECT has_table_privilege('sample_comorbidity', 'SELECT, INSERT, UPDATE, DELETE, TRUNCATE, REFERENCES'));
-  SELECT count(*) INTO column_count
-  FROM information_schema.columns
-  WHERE table_schema='sars_cov_2' AND table_name='sample_comorbidity' AND column_name IN ('sample_id','comorbidity_id');
-  ASSERT column_count = 2;
-
-    -- governorate table verifications
-  ASSERT (SELECT has_table_privilege('governorate', 'SELECT, INSERT, UPDATE, DELETE, TRUNCATE, REFERENCES'));
-  SELECT count(*) INTO column_count
-  FROM information_schema.columns
-  WHERE table_schema='sars_cov_2' AND table_name='governorate' AND column_name IN ('name','latitude','longitude','colour');
-  ASSERT column_count = 4;
 
 END $$;
