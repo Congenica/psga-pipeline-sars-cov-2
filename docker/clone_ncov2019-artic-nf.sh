eff537bf7a363f9282f1454a17d647a62d17298a
Piero Dalle Pezze <piero.dallepezze@congenica.com>
2022-10-17 16:31:21 +0100

    PSG-1168: refactory of sars-cov-2 docker images

--- docker/clone_ncov2019-artic-nf.sh
+++ docker/clone_ncov2019-artic-nf.sh
@@ -3,15 +3,18 @@
 set -e
 set -o pipefail
 
-source sars_cov_2.deps
+pathogen="sars_cov_2"
+
+source ${pathogen}/ncov.deps
 
 git clone --recursive https://github.com/Congenica/ncov2019-artic-nf.git
 cd ncov2019-artic-nf
 git checkout ${ncov_commit}
 
-cp environments/extras.yml ../ncov2019-artic-nf_extras.yml
-cp environments/illumina/environment.yml ../ncov2019-artic-nf_illumina_environment.yml
-cp environments/nanopore/environment.yml ../ncov2019-artic-nf_nanopore_environment.yml
+cp environments/extras.yml ../${pathogen}/ncov2019-artic-nf_extras.yml
+cp environments/illumina/environment.yml ../${pathogen}/ncov2019-artic-nf_illumina.yml
+cp environments/nanopore/environment.yml ../${pathogen}/ncov2019-artic-nf_nanopore.yml
+
 cd ../
 
 rm -rf ncov2019-artic-nf
