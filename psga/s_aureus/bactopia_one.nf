d45a09d4f9a8077dd954242ff982b087351622d3
john-big-dev-machine <john-big-dev-machine@congenica.com>
2022-12-05 17:27:45 +0000

    Copy lots of bioinformatics stuff to output

--- psga/s_aureus/bactopia_one.nf
+++ psga/s_aureus/bactopia_one.nf
@@ -12,16 +12,20 @@ process bactopia_one {
 
   output:
     path ch_input_files, emit: ch_input_files
-    path "hello.txt", emit: hello
+//     path "hello.txt", emit: hello
     path "metadata.csv", emit: md_out
     path "local-single-sample/bactopia/nf-reports/bactopia-trace.txt", emit: bactopia_trace
-//     path "local-single-sample/*/variants/*/*.bam", emit: bam_file
-//     path "local-single-sample/*/variants/*/*annotated.vcf.gz", emit: annotated_vcf
-//     path "local-single-sample/*/variants/*/*consensus.fa.gz", emit: consensus_fasta
-//     path "local-single-sample/9729bce7-f0a9-4617-b6e0-6145307741d1/logs/call_variants/refseq-genomes/versions.yml", emit: call_variants_versions
-//     path "local-single-sample/9729bce7-f0a9-4617-b6e0-6145307741d1/logs/antimicrobial-resistance/9729bce7-f0a9-4617-b6e0-6145307741d1-protein-report.txt", emit: antimicrobial_resistance
-    // TODO work out how to get sample name in this part of the file ... or call bactopia with a constant sample name
-    stdout
+    path "local-single-sample/*/variants/*/*.bam", emit: bam_file
+    path "local-single-sample/*/variants/*/*.vcf.gz", emit: annotated_vcf
+    path "local-single-sample/*/variants/*/*consensus.fa.gz", emit: consensus_fasta
+    path "local-single-sample/*/quality-control/summary/*final_fastqc.zip", emit: final_fastqc
+    path "local-single-sample/*/mlst/default/blast/*blast.json", emit: mlst_json
+    path "local-single-sample/*/logs/assembly_qc/checkm.log", emit: checkm_log
+    path "local-single-sample/*/assembly/checkm/bins/*/genes.gff", emit: checkm_genes_gff
+    path "local-single-sample/*/assembly/checkm/bins/*/hmmer.tree.txt", emit: checkm_hmmer_tree
+    path "local-single-sample/*/assembly/checkm/checkm-results.txt", emit: checkm_results_txt
+    path "local-single-sample/*/antimicrobial-resistance/*-report.txt", emit: antimicrobial_resistance
+    path "local-single-sample/logs/custom_dumpsoftwareversions/versions.yml", emit: software_versions
 
   script:
   """
@@ -40,7 +44,7 @@ with open('hello.txt', 'w') as outfile:
     if not sample_id:
         raise Exception('Cannot get sample_id from input filenames')
 
-    bactopia_cmd = 'bactopia --R1 {sample_id}_1.fastq.gz --R2 {sample_id}_2.fastq.gz --sample {sample_id} --datasets /datasets/ --species "Staphylococcus aureus" --coverage 100 --genome_size median --outdir local-single-sample --max_cpus 10 -qs 1 --max_retry 1'.format(
+    bactopia_cmd = 'bactopia --R1 {sample_id}_1.fastq.gz --R2 {sample_id}_2.fastq.gz --sample {sample_id} --datasets /datasets/ --species "Staphylococcus aureus" --coverage 100 --genome_size median --outdir local-single-sample --max_cpus 10 -qs 1 --max_retry 1 --run_checkm'.format(
         sample_id=sample_id)
 
     outfile.write(bactopia_cmd + " ## ")
--- scripts/s_aureus/generate_results.py
+++ scripts/s_aureus/generate_results.py
@@ -14,6 +14,36 @@ from scripts.util.logger import get_structlog_logger
 from scripts.util.metadata import EXPECTED_HEADERS as EXPECTED_METADATA_HEADERS, SAMPLE_ID, ILLUMINA, ONT
 from scripts.validation.check_csv_columns import check_csv_columns
 
+from collections import namedtuple
+
+# These are all of the files that get put in the resultfiles.json
+# TODO finish the explainers
+JsonOutFile = namedtuple('JsonOutFile', 'filepath typedescription')
+OUTPUT_FILES = [
+    # TODO where does GCF_004153365 come from?
+    JsonOutFile('{output_path}/bactopia_one/local-single-sample/logs/custom_dumpsoftwareversions/versions.yml', 'software-versions'),
+    JsonOutFile('{output_path}/bactopia_one/local-single-sample/{sample_id}/logs/assembly_qc/checkm.log', 'software-versions'),
+    JsonOutFile('{output_path}/bactopia_one/{sample_id}_1.fastq.gz', 'input-reads-1'),
+    JsonOutFile('{output_path}/bactopia_one/{sample_id}_2.fastq.gz', 'input-reads-2'),
+    JsonOutFile('{output_path}/bactopia_one/local-single-sample/{sample_id}/variants/GCF_004153365/{sample_id}.raw.vcf.gz', 'TODO'),
+    JsonOutFile('{output_path}/bactopia_one/local-single-sample/{sample_id}/variants/GCF_004153365/{sample_id}.annotated.vcf.gz', 'TODO'),
+    JsonOutFile('{output_path}/bactopia_one/local-single-sample/{sample_id}/variants/GCF_004153365/{sample_id}.filt.vcf.gz', 'TODO'),
+    JsonOutFile('{output_path}/bactopia_one/local-single-sample/{sample_id}/variants/GCF_004153365/{sample_id}.bam', 'TODO'),
+    JsonOutFile('{output_path}/bactopia_one/local-single-sample/{sample_id}/variants/GCF_004153365/{sample_id}.subs.vcf.gz', 'TODO'),
+    JsonOutFile('{output_path}/bactopia_one/local-single-sample/{sample_id}/variants/GCF_004153365/{sample_id}.vcf.gz', 'TODO'),
+    JsonOutFile('{output_path}/bactopia_one/local-single-sample/{sample_id}/variants/GCF_004153365/{sample_id}.consensus.fa.gz', 'TODO'),
+    JsonOutFile('{output_path}/bactopia_one/local-single-sample/{sample_id}/mlst/default/blast/{sample_id}-blast.json', 'TODO'),
+    JsonOutFile('{output_path}/bactopia_one/local-single-sample/{sample_id}/antimicrobial-resistance/{sample_id}-protein-report.txt', 'TODO'),
+    JsonOutFile('{output_path}/bactopia_one/local-single-sample/{sample_id}/antimicrobial-resistance/{sample_id}-gene-report.txt', 'TODO'),
+    JsonOutFile('{output_path}/bactopia_one/local-single-sample/{sample_id}/quality-control/summary/{sample_id}_R2-final_fastqc.zip', 'TODO'),
+    JsonOutFile('{output_path}/bactopia_one/local-single-sample/{sample_id}/quality-control/summary/{sample_id}_R1-final_fastqc.zip', 'TODO'),
+    JsonOutFile('{output_path}/bactopia_one/local-single-sample/{sample_id}/assembly/checkm/bins/{sample_id}/hmmer.tree.txt', 'TODO'),
+    JsonOutFile('{output_path}/bactopia_one/local-single-sample/{sample_id}/assembly/checkm/bins/{sample_id}/genes.gff', 'TODO'),
+    JsonOutFile('{output_path}/bactopia_one/local-single-sample/{sample_id}/assembly/checkm/checkm-results.txt', 'TODO'),
+    JsonOutFile('{output_path}/bactopia_one/local-single-sample/bactopia/nf-reports/bactopia-trace.txt', 'bactopia-run-info'),
+]
+
+
 @click.command()
 @click.option(
     "--metadata-file",
@@ -68,6 +98,7 @@ def generate_results(
         writer = csv.writer(csv_out)
         writer.writerow(['SAMPLE_ID', 'STATUS'])
         for sample in metadata:
+            # For the demo everything passes TODO come back to this
             writer.writerow([sample['SAMPLE_ID'], 'PASSED'])
 
     json_out = dict()
@@ -78,6 +109,11 @@ def generate_results(
             "file": "{output_path}/bactopia_one/local-single-sample/bactopia/nf-reports/bactopia-trace.txt".format(output_path=output_path),
             "type": "nextflow-run-log"
         })
+        for of in OUTPUT_FILES:
+            out_files.append({
+                "file": of.filepath.format(output_path=output_path, sample_id=sample['SAMPLE_ID']),
+                "type": of.typedescription
+            })
 
         json_out[sample['SAMPLE_ID']] = out_files
 
