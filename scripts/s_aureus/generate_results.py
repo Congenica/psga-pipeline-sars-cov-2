cc77141a13fe276991b4bdf6511196393030e2d7
john-big-dev-machine <john-big-dev-machine@congenica.com>
2022-12-14 23:13:05 +0000

    generate the per channel results files in the channel so nextflow won't mix up the results

--- scripts/s_aureus/generate_results.py
+++ scripts/s_aureus/generate_results.py
@@ -50,7 +50,7 @@ OUTPUT_FILES = [
 ]
 
 
-def build_csv_list(sample_id):
+def build_csv_list(sample_id, genome_assembly_name):
     """
     Gather all info for the CSV file
     Make a defined list of files that we are expecting.
@@ -62,7 +62,7 @@ def build_csv_list(sample_id):
     csv_list.append(CsvItem('STATUS', 'Completed'))  # For the demo everything passes
     csv_list.append(CsvItem('QC_STATUS', 'pass'))  # The key QC_STATUS gets shown in the UI
 
-    with open('software_versions.yml') as software_versions:
+    with open('local-single-sample/bactopia-tools/mykrobe/mykrobe/{sample_id}/logs/mykrobe/versions.yml'.format(sample_id=sample_id)) as software_versions:
         versions = list(csv.reader(software_versions, delimiter=":"))
         for row in versions:
             if 'checkm' in row[0]:
@@ -72,45 +72,45 @@ def build_csv_list(sample_id):
             if 'fastqc' in row[0]:
                 csv_list.append(CsvItem('{}-version'.format(row[0].rstrip().lstrip()), row[1].lstrip().rstrip()))
 
-    with open('mlst_versions.yml') as mlst_versions:
+    with open('local-single-sample/bactopia-tools/mlst/mlst/software_versions.yml') as mlst_versions:
         m_versions = list(csv.reader(mlst_versions, delimiter=":"))
         for row in m_versions:
             if 'mlst' in row[0]:
                 csv_list.append(CsvItem('{}-version'.format(row[0].rstrip().lstrip()), row[1].lstrip().rstrip()))
 
-    with open('mykrobe_versions.yml') as mykrobe_versions:
+    with open('local-single-sample/bactopia-tools/mykrobe/mykrobe/{sample_id}/logs/mykrobe/versions.yml'.format(sample_id=sample_id)) as mykrobe_versions:
         myk_versions = list(csv.reader(mykrobe_versions, delimiter=":"))
         for row in myk_versions:
             if 'mykrobe' in row[0]:
                 csv_list.append(CsvItem('{}-version'.format(row[0].rstrip().lstrip()), row[1].lstrip().rstrip()))
 
-    with open('annotation-summary.txt') as annotation_summary:
+    with open('sample-{sample_id}-csv-files/annotation-summary.txt'.format(sample_id=sample_id)) as annotation_summary:
         annotations = csv.reader(annotation_summary, delimiter =":")
         for row in annotations:
             csv_list.append(CsvItem('Annotation-{}'.format(row[0]), row[1].lstrip().rstrip()))
 
     # Variant summary
-    with open('{}.txt'.format(sample_id)) as variant_summary_file:
+    with open('local-single-sample/{sample_id}/variants/{genome_assembly_name}/{sample_id}.txt'.format(sample_id=sample_id, genome_assembly_name=genome_assembly_name)) as variant_summary_file:
         variant_summary = csv.reader(variant_summary_file, delimiter="\t")
         for row in variant_summary:
             if 'Variant' in row[0]:
                 csv_list.append(CsvItem(row[0].rstrip().lstrip(), row[1].lstrip().rstrip()))
 
-    with open('checkm-results.txt') as checkm_results_file:
+    with open('local-single-sample/{sample_id}/assembly/checkm/checkm-results.txt'.format(sample_id=sample_id)) as checkm_results_file:
         checkm_results = next(csv.DictReader(checkm_results_file, delimiter="\t"))
         csv_list.append(CsvItem('Marker lineage', checkm_results['Marker lineage']))
         csv_list.append(CsvItem('Completeness', checkm_results['Completeness']))
         csv_list.append(CsvItem('Contamination', checkm_results['Contamination']))
         csv_list.append(CsvItem('Strain heterogeneity', checkm_results['Strain heterogeneity']))
 
-    with open('assembly.json') as assembly_results_file:
+    with open('local-single-sample/{sample_id}/assembly/{sample_id}.json'.format(sample_id=sample_id)) as assembly_results_file:
         assembly_results = json.loads(assembly_results_file.read())
         for key, value in assembly_results.items():
             if key == 'fasta':
                 continue
             csv_list.append(CsvItem(key , value))
 
-    with open('{sample_id}-protein-report.txt'.format(sample_id=sample_id)) as antimicrobial_protein_file:
+    with open('local-single-sample/{sample_id}/antimicrobial-resistance/{sample_id}-protein-report.txt'.format(sample_id=sample_id)) as antimicrobial_protein_file:
         resistance_genes = list()
         for row in csv.DictReader(antimicrobial_protein_file, delimiter="\t"):
             resistance_genes.append(row['Gene symbol'])
@@ -141,7 +141,7 @@ def generate_results(
     print('genome_assembly_name: ' + genome_assembly_name)
     print('sample_id: ' + sample_id)
 
-    csv_list = build_csv_list(sample_id)
+    csv_list = build_csv_list(sample_id, genome_assembly_name)
 
     with open('{sample_id}-results.csv'.format(sample_id=sample_id), 'w', encoding='UTF8') as csv_out:
         writer = csv.writer(csv_out)
