f6f0a22d0eac31c44049cfd9962638c019881d0c
Chris Ramshaw <chris.ramshaw@congenica.com>
2023-12-13 10:20:22 +0000

    reformat

--- tests/common/test_check_metadata.py
+++ tests/common/test_check_metadata.py
@@ -41,9 +41,7 @@ from tests.util.test_notification import load_log_file_to_dict
         ),
     ],
 )
-@pytest.mark.jira(
-    identifier="5ef0808f-a41a-4ea9-abb1-c46d620a0247", confirms="PSG-3621"
-)
+@pytest.mark.jira(identifier="5ef0808f-a41a-4ea9-abb1-c46d620a0247", confirms="PSG-3621")
 def test_validate_metadata(
     check_metadata_data_path: Path,
     metadata_file: str,
@@ -51,9 +49,7 @@ def test_validate_metadata(
     valid_samples: list[str],
     invalid_samples: list[str],
 ):
-    samples = validate_metadata(
-        check_metadata_data_path / metadata_file, sequencing_technology
-    )
+    samples = validate_metadata(check_metadata_data_path / metadata_file, sequencing_technology)
 
     assert sorted(samples.valid) == sorted(valid_samples)
     assert sorted(samples.invalid) == sorted(invalid_samples)
@@ -250,9 +246,7 @@ def test_validate_metadata(
         ),
     ],
 )
-@pytest.mark.jira(
-    identifier="53c2d945-92e5-4f92-b256-3891fca6bb18", confirms="PSG-3621"
-)
+@pytest.mark.jira(identifier="53c2d945-92e5-4f92-b256-3891fca6bb18", confirms="PSG-3621")
 def test_check_metadata(
     tmp_path: Path,
     check_metadata_data_path: Path,
@@ -296,13 +290,9 @@ def test_check_metadata(
         log_dict = load_log_file_to_dict(log_file, "sample")
 
         if valid_samples is not None:
-            processed_valid_samples = [
-                k for k, v in log_dict.items() if v["level"] == "info"
-            ]
+            processed_valid_samples = [k for k, v in log_dict.items() if v["level"] == "info"]
             assert sorted(valid_samples) == sorted(processed_valid_samples)
 
         if invalid_samples is not None:
-            processed_invalid_samples = [
-                k for k, v in log_dict.items() if v["level"] == "error"
-            ]
+            processed_invalid_samples = [k for k, v in log_dict.items() if v["level"] == "error"]
             assert sorted(invalid_samples) == sorted(processed_invalid_samples)
