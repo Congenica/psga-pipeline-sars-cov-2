6eb7506a347ad762c231e3c11870e716ea4d62b5
Chris Ramshaw <chris.ramshaw@gmail.com>
2024-02-21 23:06:12 +0000

    put back

--- minikube/run_pipelines.sh
+++ minikube/run_pipelines.sh
@@ -8,7 +8,7 @@ PSGA_ROOT_PATH="/app"
 for name in $PIPELINES; do
   for config in $(ls test_configs/$name); do
     echo "Running $name/$config test"
-    source test_configs/$name/ont_unknown
+    source test_configs/$name/$config
     pathogen="${name//-/_}"
     pipeline_pod="$( kubectl get pods -l app=$name-pipeline-minikube --no-headers -o custom-columns=':metadata.name' )"
     kubectl -n psga-minikube exec $pipeline_pod -- bash -c "AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} nextflow -log ${ANALYSIS_RUN}.log run ${PSGA_ROOT_PATH}/main_new.nf --run ${ANALYSIS_RUN} --sequencing_technology ${SEQUENCING_TECHNOLOGY} --kit ${KIT} --config-path ${CONFIG_PATH} --output_path ${OUTPUT_PATH}"
